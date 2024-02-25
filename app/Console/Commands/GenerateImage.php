78e836bba7d792f0ea59dfa4160b44ee9b9d1a89
Alfred Nutile <alfrednutile@gmail.com>
2024-02-24 21:21:30 -0500

    add generate

---
+++ app/Console/Commands/GenerateImage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Gallery;
+use Facades\App\OpenAi\GenerateImage as OpenAiGenerateImage;
+use Illuminate\Console\Command;
+
+class GenerateImage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-image';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Yup';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $gallery = Gallery::first();
+        $image = GenerateImage::generate($gallery);
+    }
+}
